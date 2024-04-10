53073d0d42cc5333180a7064dc0e78edea5377ea
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-04-10 01:02:04 -0500

    fix: capture traces

--- test/Blog.Tests/EndToEnd/BlogTest.cs
+++ test/Blog.Tests/EndToEnd/BlogTest.cs
@@ -14,4 +14,29 @@ public class BlogTest : PageTest
     options.BaseURL = _factory.ServerAddress;
     return options;
   }
+
+  [SetUp]
+  public async Task Setup()
+  {
+    await Context.Tracing.StartAsync(new()
+    {
+      Title = TestContext.CurrentContext.Test.ClassName + "." + TestContext.CurrentContext.Test.Name,
+      Screenshots = true,
+      Snapshots = true,
+      Sources = true
+    });
+  }
+
+  [TearDown]
+  public async Task TearDown()
+  {
+    await Context.Tracing.StopAsync(new()
+    {
+        Path = Path.Combine(
+            TestContext.CurrentContext.WorkDirectory,
+            "playwright-traces",
+            $"{TestContext.CurrentContext.Test.ClassName}.{TestContext.CurrentContext.Test.Name}.zip"
+        )
+    });
+  }
 }
